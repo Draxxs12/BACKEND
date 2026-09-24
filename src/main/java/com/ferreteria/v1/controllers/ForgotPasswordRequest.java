36ec1d22947610bb7fe83e96a1abe8b1d6390681
Draxxs12 <midanale12@gmail.com>
2026-09-24 12:27:26 -0500

    feat(auth): add email verification challenge infrastructure

---
+++ src/main/java/com/ferreteria/v1/controllers/ForgotPasswordRequest.java
@@ -0,0 +1,7 @@
+package com.ferreteria.v1.controllers;
+
+public class ForgotPasswordRequest {
+    private String email;
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+}
