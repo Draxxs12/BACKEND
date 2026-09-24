5995552b61c58eb5bc8fbfb84400543280ded7fd
Draxxs12 <midanale12@gmail.com>
2026-09-24 13:36:00 -0500

    fix(email): corregir import de HttpCredentialsAdapter

--- src/main/java/com/ferreteria/v1/services/EmailService.java
+++ src/main/java/com/ferreteria/v1/services/EmailService.java
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
-import com.google.api.client.http.HttpCredentialsAdapter;
+import com.google.auth.http.HttpCredentialsAdapter;
 import com.google.api.client.json.gson.GsonFactory;
 import com.google.api.services.gmail.Gmail;
 import com.google.api.services.gmail.GmailScopes;
