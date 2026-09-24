961be7267f8de4470c35bdbf44a51ba839e0a1db
Draxxs12 <midanale12@gmail.com>
2026-09-24 14:49:01 -0500

    feat(perfil): agregar request para cambio de contraseña

---
+++ src/main/java/com/ferreteria/v1/controllers/ChangePasswordRequest.java
@@ -0,0 +1,13 @@
+package com.ferreteria.v1.controllers;
+
+public class ChangePasswordRequest {
+    private String currentPassword;
+
+    public String getCurrentPassword() {
+        return currentPassword;
+    }
+
+    public void setCurrentPassword(String currentPassword) {
+        this.currentPassword = currentPassword;
+    }
+}
